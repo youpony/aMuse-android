7708b7edd41eaaaa03016214ce62d7926a28b6cd
Leonardo Lanzinger <leonardo.lanzinger@gmail.com>
2013-03-25 14:25:54 +0100

    Added ListView on Story.java

--- src/com/youpony/amuse/Story.java
+++ src/com/youpony/amuse/Story.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -17,14 +18,26 @@ import android.widget.TextView;
 public class Story extends Fragment {
 	public Story(){
 	}
-
+	
+	ListView lv1;
+	
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         //inflate the layout to fragment
 		View sView = inflater.inflate(R.layout.activity_story, container, false);
 		//set custom list view for fragment
-		ListView lista;
+		lv1 = (ListView) sView.findViewById(R.id.listView1);    
+
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+             "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+        "Linux", "OS/2" };
+
+        ArrayAdapter<String> files = new ArrayAdapter<String>(getActivity(), 
+                 android.R.layout.simple_list_item_1, 
+                 values);
+
+         lv1.setAdapter(files);
 		
         return sView;
     }
